637c80d0fd8d8bd8b2a57178c8c41bb60c04debc
Lesly Karina Dubón Cruz <lkdubonc@outlook.com>
2022-06-28 08:14:38 -0600

    Agregar archivos de proyecto.

---
+++ PruebaF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
